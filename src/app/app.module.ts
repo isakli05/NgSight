28872829440abdd9fdcf8e8aa2ed560f2ac28048
isakli05 <isakaya709@gmail.com>
2019-09-12 07:57:07 +0300

    Create bar chart, directory, structure, update styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { RouterModule } from '@angular/router';
 import { appRoutes } from '../routes';
 
 import { NgModule } from '@angular/core';
+import {ChartsModule} from 'ng2-charts';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -11,6 +12,9 @@ import { SidebarComponent } from './sidebar/sidebar.component';
 import { SectionSalesComponent } from './sections/section-sales/section-sales.component';
 import { SectionOrdersComponent } from './sections/section-orders/section-orders.component';
 import { SectionHealtComponent } from './sections/section-healt/section-healt.component';
+import { BarChartComponent } from './charts/bar-chart/bar-chart.component';
+import { LineChartComponent } from './charts/line-chart/line-chart.component';
+import { PieChartComponent } from './charts/pie-chart/pie-chart.component';
 
 @NgModule({
   declarations: [
@@ -19,12 +23,17 @@ import { SectionHealtComponent } from './sections/section-healt/section-healt.co
     SidebarComponent,
     SectionSalesComponent,
     SectionOrdersComponent,
-    SectionHealtComponent
+    SectionHealtComponent,
+    BarChartComponent,
+    LineChartComponent,
+    PieChartComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    ChartsModule
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
